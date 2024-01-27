13a795b1b16ffe9f727d20cd9fead83743133426
Stevenix <Stephenix26@gmail.com>
2024-01-27 14:00:22 +0100

    made chages

--- src/app/server/api/root.ts
+++ src/app/server/api/root.ts
@@ -1,14 +1,14 @@
-import { createTRPCRouter } from "../api/trpc";
-import { prescribeRouter} from "./routers/precribes"
+// import { createTRPCRouter } from "../api/trpc";
+// import { prescribeRouter} from "./routers/precribes"
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
-export const appRouter = createTRPCRouter({
-  prescribes: prescribeRouter,
-});
+// /**
+//  * This is the primary router for your server.
+//  *
+//  * All routers added in /api/routers should be manually added here.
+//  */
+// export const appRouter = createTRPCRouter({
+//   prescribes: prescribeRouter,
+// });
 
-// export type definition of API
-export type AppRouter = typeof appRouter;
+// // export type definition of API
+// export type AppRouter = typeof appRouter;
--- src/app/server/api/routers/users.ts
+++ src/app/server/api/routers/users.ts
@@ -1,21 +1,22 @@
-import { user } from "@nextui-org/react";
-import { PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from 'next';
+// import { user } from "@nextui-org/react";
+// import { PrismaClient } from "@prisma/client";
+// import { NextApiRequest, NextApiResponse } from 'next';
 
-const prisma = new PrismaClient();
-
-
-export default async(req: NextApiRequest,res: NextApiResponse)=>{
-    if (req.method !== 'POST'){
-        return res.status(405).json({ message: 'Method not allowwed'});
-    }
-   const userData = JSON.parse(req.body);
-
-   const savedUser = await prisma.user.create({
-    data: userData
-   })
+// const prisma = new PrismaClient();
 
 
-   res.json(savedUser);
+// // eslint-disable-next-line import/no-anonymous-default-export
+// export default async(req: NextApiRequest,res: NextApiResponse)=>{
+//     if (req.method !== 'POST'){
+//         return res.status(405).json({ message: 'Method not allowwed'});
+//     }
+//    const userData = JSON.parse(req.body);
 
-}
+//    const savedUser = await prisma.user.create({
+//     data: userData
+//    })
+
+
+//    res.json(savedUser);
+
+// }
