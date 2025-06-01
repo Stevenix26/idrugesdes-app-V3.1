52c597a90d494854e0705554ffe56afcf683521f
Stevenix <Stephenix26@gmail.com>
2025-06-01 21:53:31 +0100

    Improvement

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -107,7 +107,7 @@ export default function ClientLayout({
                 href="/store"
                 className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300"
               >
-                Stores
+                Store
               </Link>
               <Link
                 href="/dashboard"
